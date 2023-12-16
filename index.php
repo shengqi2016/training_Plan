6f7a0f172c6adae09004566e106715e7dc38436a
shengqi2016 <shengqi2016@gmail.com>
2023-12-16 19:22:03 -0400

    Test_12_6_2023

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+/*
+ * Author: Qi Sheng
+ * Date: 2022-12-01
+ * Class: CIS-2288
+ * Description: Practical Final 1
+ */
+?>
+<!doctype html>
+<html>
+<head>
+    <title>Training hours</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+        
+    body {
+  background-color: lightblue;
+    }
+    </style>
+</head>
+<body>
+    <div class="container">
+    <h1>
+        陆宇星大傻子
+    </h1>
+    </div>
+</body>
+
+
+</html>
